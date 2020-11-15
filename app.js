9df16a3832797be2fd408562adf294ea01335e3c
CobraSlasher <52565468+CobraSlasher@users.noreply.github.com>
2020-11-16 00:01:34 +0100

    second commit

---
+++ app.js
@@ -0,0 +1,9 @@
+let express = require('express')
+let app = express()
+let router = require('./routes')
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'));
+
+app.use('/', router)
+
+app.listen(process.env.PORT || 3000)
