5a64744619231fc4be03b03a27777862508283c9
amed296 <ali.medani296@gmail.com>
2020-11-16 23:11:00 +0100

    ready again f

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let express = require('express')
 let app = express()
 let router = require('./routes')
-
+console.log("Hello app")
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'));
 
