0bd7d9ddf5ceb6625bb980f33bf7d98db950ccba
amed296 <ali.medani296@gmail.com>
2020-11-16 18:44:44 +0100

    added cors

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 let express = require('express')
 let app = express()
 let router = require('./routes')
+let cors = require('cors')
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'));
-
+app.use(cors())
 app.use('/', router)
 
 app.listen(process.env.PORT || 3000)
